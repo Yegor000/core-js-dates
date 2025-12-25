34e31adb406b67d3e4a70daf8a5038d6a99542a8
Yegor000 <122170879+Yegor000@users.noreply.github.com>
2025-12-26 02:49:08 +0300

    feat: implement getCountDaysOnPeriod function

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -84,8 +84,7 @@ function getNextFriday(date) {
  * 2, 2024 => 29
  */
 function getCountDaysInMonth(month, year) {
-  const d = new Date(year, month);
-  d.setDate(d.getDate() - 1);
+  const d = new Date(year, month, 0);
   return d.getDate();
 }
 
@@ -100,8 +99,10 @@ function getCountDaysInMonth(month, year) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const d1 = new Date(dateStart);
+  const d2 = new Date(dateEnd);
+  return Math.floor((d2 - d1) / (24 * 60 * 60 * 1000)) + 1;
 }
 
 /**
